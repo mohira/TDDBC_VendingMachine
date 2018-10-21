d41564b371fa7612636cd1e3ccdfbb1792db9e81
mohira <shukatsudaison@gmail.com>
2018-10-21 18:05:37 +0900

    烏龍茶ボタンと炭酸水ボタンを追加

--- test_vending_machine.py
+++ test_vending_machine.py
@@ -10,11 +10,17 @@ class VendingMachine:
     def __init__(self):
         self.deposit: int = 0
 
+    def insert(self, coin: Coin) -> None:
+        self.deposit += coin.value
+
     def push_コーラボタン(self) -> str:
         return "コーラ"
 
-    def insert(self, coin: Coin) -> None:
-        self.deposit += coin.value
+    def push_烏龍茶ボタン(self) -> str:
+        return "烏龍茶"
+
+    def push_炭酸水ボタン(self) -> str:
+        return "炭酸水"
 
 
 class TestVendingMachine(unittest.TestCase):
@@ -27,6 +33,14 @@ class TestVendingMachine(unittest.TestCase):
     def test_お題2_100円コインを投入してからコーラボタンを押すとコーラが出る(self):
         self.vending_machine.insert(Coin.yen100)
         self.assertEqual("コーラ", self.vending_machine.push_コーラボタン())
+
+    def test_お題3_100円コインを投入してから烏龍茶ボタンを押すと烏龍茶が出る(self):
+        self.vending_machine.insert(Coin.yen100)
+        self.assertEqual("烏龍茶", self.vending_machine.push_烏龍茶ボタン())
+
+    def test_お題3_100円コインを投入してから炭酸水ボタンを押すと炭酸水が出る(self):
+        self.vending_machine.insert(Coin.yen100)
+        self.assertEqual("炭酸水", self.vending_machine.push_炭酸水ボタン())
 
 
 if __name__ == "__main__":
