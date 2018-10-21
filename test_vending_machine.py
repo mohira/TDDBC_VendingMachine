faadf27d97b40e2e4aa5def78d535bd34f9ed613
mohira <shukatsudaison@gmail.com>
2018-10-21 18:21:20 +0900

    飲料の価格という概念を追加

--- test_vending_machine.py
+++ test_vending_machine.py
@@ -10,12 +10,18 @@ class Coin(Enum):
 @dataclass(frozen=True)
 class Beverage:
     name: str
+    price: int
 
 
 class BeverageCollection(Enum):
-    コーラ = Beverage("コーラ")
-    炭酸水 = Beverage("炭酸水")
-    烏龍茶 = Beverage("烏龍茶")
+    コーラ = Beverage(name="コーラ", price=100)
+    炭酸水 = Beverage(name="炭酸水", price=100)
+    烏龍茶 = Beverage(name="烏龍茶", price=100)
+    レッドブル = Beverage(name="レッドブル", price=200)
+
+
+class 残高不足Error(Exception):
+    pass
 
 
 class VendingMachine:
@@ -26,13 +32,33 @@ class VendingMachine:
         self.deposit += coin.value
 
     def push_コーラボタン(self) -> Beverage:
-        return BeverageCollection.コーラ
+        コーラ = BeverageCollection.コーラ
+        if self.deposit < コーラ.value.price:
+            raise 残高不足Error("コーラは買えないよ")
+
+        return コーラ
 
     def push_烏龍茶ボタン(self) -> Beverage:
-        return BeverageCollection.烏龍茶
+        烏龍茶 = BeverageCollection.烏龍茶
+        if self.deposit < 烏龍茶.value.price:
+            raise 残高不足Error("烏龍茶は買えないよ")
+
+        return 烏龍茶
 
     def push_炭酸水ボタン(self) -> Beverage:
-        return BeverageCollection.炭酸水
+        炭酸水 = BeverageCollection.炭酸水
+        if self.deposit < 炭酸水.value.price:
+            raise 残高不足Error("烏龍茶は買えないよ")
+
+        return 炭酸水
+
+    def push_レッドブルボタン(self) -> Beverage:
+        レッドブル = BeverageCollection.レッドブル
+
+        if self.deposit < レッドブル.value.price:
+            raise 残高不足Error("レッドブルは買えないよ")
+
+        return レッドブル
 
 
 class TestVendingMachine(unittest.TestCase):
@@ -51,6 +77,25 @@ class TestVendingMachine(unittest.TestCase):
         self.vending_machine.insert(Coin.yen100)
 
         self.assertEqual(BeverageCollection.炭酸水, self.vending_machine.push_炭酸水ボタン())
+
+    def test_お題4_200円の残高があるときにレッドブルボタンを押すとレッドブルが出る(self):
+        self.vending_machine.insert(Coin.yen100)
+        self.vending_machine.insert(Coin.yen100)
+
+        self.assertEqual(BeverageCollection.レッドブル, self.vending_machine.push_レッドブルボタン())
+
+    def test_残高不足の場合の対応(self):
+        with self.subTest("200円以上の残高がないとレッドブルボタンを押してもレッドブルは出ない"):
+            self.assertRaises(残高不足Error, lambda: self.vending_machine.push_レッドブルボタン())
+
+        with self.subTest("100円以上の残高がないとコーラボタンを押してもコーラは出ない"):
+            self.assertRaises(残高不足Error, lambda: self.vending_machine.push_コーラボタン())
+
+        with self.subTest("200円以上の残高がないと烏龍茶ボタンを押しても烏龍茶は出ない"):
+            self.assertRaises(残高不足Error, lambda: self.vending_machine.push_烏龍茶ボタン())
+
+        with self.subTest("200円以上の残高がないと炭酸水ボタンを押しても炭酸水は出ない"):
+            self.assertRaises(残高不足Error, lambda: self.vending_machine.push_炭酸水ボタン())
 
 
 if __name__ == "__main__":
