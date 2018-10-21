bbb9e9cecdda613a6ac57774e46c336fa35506df
mohira <shukatsudaison@gmail.com>
2018-10-21 18:02:53 +0900

    100円コインを投入してからコーラボタンを押すとコーラが出る

--- test_vending_machine.py
+++ test_vending_machine.py
@@ -1,17 +1,32 @@
 import unittest
+from enum import Enum
+
+
+class Coin(Enum):
+    yen100 = 100
 
 
 class VendingMachine:
+    def __init__(self):
+        self.deposit = 0
+
     def push_コーラボタン(self) -> str:
-        return "コーラ "
+        return "コーラ"
+
+    def insert(self, coin: Coin) -> None:
+        self.deposit += coin.value
 
 
 class TestVendingMachine(unittest.TestCase):
-    def test_お題1_コーラボタンを押すとコーラが出る(self):
-        vending_machine = VendingMachine()
-        actual = vending_machine.push_コーラボタン()
+    def setUp(self):
+        self.vending_machine = VendingMachine()
 
-        self.assertEqual("コーラ", actual)
+    def test_お題1_コーラボタンを押すとコーラが出る(self):
+        self.assertEqual("コーラ", self.vending_machine.push_コーラボタン())
+
+    def test_お題2_100円コインを投入してからコーラボタンを押すとコーラが出る(self):
+        self.vending_machine.insert(Coin.yen100)
+        self.assertEqual("コーラ", self.vending_machine.push_コーラボタン())
 
 
 if __name__ == "__main__":
