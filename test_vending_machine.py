2e12c8c3d4ec35a3ac5eb1ea5e7a8b81ca259b21
mohira <shukatsudaison@gmail.com>
2018-10-21 18:12:04 +0900

    BeverageとBeverageCollectionという概念を追加
    - お題1のテストは削除
      - お題2のテストに包含されていると判断したため

--- test_vending_machine.py
+++ test_vending_machine.py
@@ -1,9 +1,21 @@
 import unittest
+from dataclasses import dataclass
 from enum import Enum
 
 
 class Coin(Enum):
     yen100 = 100
+
+
+@dataclass(frozen=True)
+class Beverage:
+    name: str
+
+
+class BeverageCollection(Enum):
+    コーラ = Beverage("コーラ")
+    炭酸水 = Beverage("炭酸水")
+    烏龍茶 = Beverage("烏龍茶")
 
 
 class VendingMachine:
@@ -13,34 +25,32 @@ class VendingMachine:
     def insert(self, coin: Coin) -> None:
         self.deposit += coin.value
 
-    def push_コーラボタン(self) -> str:
-        return "コーラ"
+    def push_コーラボタン(self) -> Beverage:
+        return BeverageCollection.コーラ
 
-    def push_烏龍茶ボタン(self) -> str:
-        return "烏龍茶"
+    def push_烏龍茶ボタン(self) -> Beverage:
+        return BeverageCollection.烏龍茶
 
-    def push_炭酸水ボタン(self) -> str:
-        return "炭酸水"
+    def push_炭酸水ボタン(self) -> Beverage:
+        return BeverageCollection.炭酸水
 
 
 class TestVendingMachine(unittest.TestCase):
     def setUp(self):
         self.vending_machine = VendingMachine()
 
-    def test_お題1_コーラボタンを押すとコーラが出る(self):
-        self.assertEqual("コーラ", self.vending_machine.push_コーラボタン())
-
     def test_お題2_100円コインを投入してからコーラボタンを押すとコーラが出る(self):
         self.vending_machine.insert(Coin.yen100)
-        self.assertEqual("コーラ", self.vending_machine.push_コーラボタン())
+        self.assertEqual(BeverageCollection.コーラ, self.vending_machine.push_コーラボタン())
 
     def test_お題3_100円コインを投入してから烏龍茶ボタンを押すと烏龍茶が出る(self):
         self.vending_machine.insert(Coin.yen100)
-        self.assertEqual("烏龍茶", self.vending_machine.push_烏龍茶ボタン())
+        self.assertEqual(BeverageCollection.烏龍茶, self.vending_machine.push_烏龍茶ボタン())
 
     def test_お題3_100円コインを投入してから炭酸水ボタンを押すと炭酸水が出る(self):
         self.vending_machine.insert(Coin.yen100)
-        self.assertEqual("炭酸水", self.vending_machine.push_炭酸水ボタン())
+
+        self.assertEqual(BeverageCollection.炭酸水, self.vending_machine.push_炭酸水ボタン())
 
 
 if __name__ == "__main__":
